609d7f51ce86e2c4682f1a3cc65bd2ec2559663d
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-09-19 12:33:09 -0500

    Initial Commit

---
+++ YouAreAUsome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAUsome
+//
+//  Created by artsw0rd on 9/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAUsome/YouAreAUsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAUsomeApp.swift
+//  YouAreAUsome
+//
+//  Created by artsw0rd on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAUsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
