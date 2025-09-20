ae4bf2f3a10e3d25c0dca3a7deda3e83e89c966c
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-09-20 01:41:33 -0500

    Changed programmer to Developer

--- YouAreAUsome/ContentView.swift
+++ YouAreAUsome/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a programmer!"
+    @State private var message = "I am a Developer!"
     var body: some View {
         VStack {
             Image(systemName: "swift")
