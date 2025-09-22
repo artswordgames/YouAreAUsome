bfbe6af0369147a31dd884954037d6e23f9e8aa6
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-09-21 21:03:30 -0500

    Update app to Single Button Version with constants for String literals

--- YouAreAUsome/ContentView.swift
+++ YouAreAUsome/ContentView.swift
@@ -8,16 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a Programmer!"
+    // Struct-wide variables
+    @State private var message = ""
+    @State private var imageName = ""
+    
+    // Views
     var body: some View {
         VStack {
             Spacer()
             
-            Image(systemName: "swift")
+            Image(systemName: imageName)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
@@ -26,13 +29,21 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack {
-                Button("Awesome!") {
-                    message = "Awesome!"
+            Button("Press Me") {
+                // Local Variables
+                let awesomeMessage = "You Are Awesome!" // Use 'let' to create constants
+                let greatMessage = "You Are Great!"
+                let thumbsUpImage = "hand.thumbsup"
+                let sunImage = "sun.max.fill"
+                
+                if message == awesomeMessage {
+                    message = greatMessage
+                    imageName = thumbsUpImage
+                } else {
+                    message = awesomeMessage
+                    imageName = sunImage
                 }
-                Button("Great!") {
-                    message = "Great!"
-                }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
